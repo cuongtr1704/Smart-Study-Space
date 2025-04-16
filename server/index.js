f6af0e2b72c778cc167bcde9ce4c73dccf7f18b9
cuongTr <131869726+cuongtr1704@users.noreply.github.com>
2025-04-16 12:38:13 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,9 +18,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
+  origin: 'https://smart-study-space.netlify.app',
+  credentials: true,
+}))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routers);
