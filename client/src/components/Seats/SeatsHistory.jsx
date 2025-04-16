d85c8b222274e1572420f9d2fe92688bab7489d5
cuongTr <131869726+cuongtr1704@users.noreply.github.com>
2025-04-16 22:18:25 +0700

    Update SeatsHistory.jsx

--- client/src/components/Seats/SeatsHistory.jsx
+++ client/src/components/Seats/SeatsHistory.jsx
@@ -38,7 +38,7 @@ export default function SeatsHistory() {
   return (
     <section className="content">
       {seatHistory.length > 0 ? (
-        <div className="flex flex-wrap gap-6 justify-start">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {seatHistory.map((book) => (
             <Item key={book._id} booking={book} />
           ))}
