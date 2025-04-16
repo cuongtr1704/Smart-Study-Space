403ee22b6e64baa2786904335e5016181b68c89d
cuongTr <131869726+cuongtr1704@users.noreply.github.com>
2025-04-16 12:49:35 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,32 +2,41 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const { errorHandler } = require('./core/errorHandler');
 const connectDB = require('./config/db');
 const scheduleExpired = require('./config/checkExpired');
 const scheduleOverdate = require('./config/checkOverdate');
 const routers = require('./routes/index');
+
 const port = process.env.PORT || 5000;
 
 connectDB();
 scheduleExpired();
 scheduleOverdate();
 
-const cors = require('cors');
-
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://smart-study-space.netlify.app' 
+];
+
 app.use(cors({
-  origin: 'https://smart-study-space.netlify.app',
+  origin: allowedOrigins,
   credentials: true,
-}))
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
 app.use(routers);
 
 app.get('/', (req, res) => res.send('API is running...'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.listen(port, () => console.log(`✅ Server started on port ${port}`));
