70ce153169518aa87a540d167e17857228e2bee7
cuongTr <131869726+cuongtr1704@users.noreply.github.com>
2025-04-16 12:30:31 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,17 +25,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routers);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
-
 app.get('/', (req, res) => res.send('API is running...'));
 
 app.use(errorHandler);
