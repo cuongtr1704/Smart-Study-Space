e827948ff65e7945b700668a2a3cf9da217bc51d
cuongTr <131869726+cuongtr1704@users.noreply.github.com>
2025-04-16 23:33:58 +0700

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import '../index.css'
+import "../index.css";
 import { useDispatch, useSelector } from "react-redux";
 import logo from "../images/logo.png";
 import { Link, useNavigate } from "react-router-dom";
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
 
 export default function Login() {
-  const [formData, setformData] = useState({
+  const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
@@ -35,7 +35,7 @@ export default function Login() {
   }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
-    setformData((prevState) => ({
+    setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
@@ -67,31 +67,34 @@ export default function Login() {
             REGISTER
           </Link>
         </div>
-        <div className="w-full lg:w-auto flex gap-0 lg:gap-40 flex-col lg:flex-row items-center justify-center">
-          <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
-            <div className="w-full lg:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 lg:gap-y-10 2xl:mt-20">
-              <p className="flex flex-col gap-0 lg:gap-4 text-4xl lg:text-6xl 2xl:text-7xl font-extrabold text-center text-black dark:text-white">
-                <span>Smart Study Space</span>
+        <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+          <div className="w-full flex flex-col items-center justify-center px-4 py-10 lg:w-2/3">
+            <div className="w-full max-w-fit flex flex-col items-center justify-center gap-5 lg:gap-y-10 2xl:mt-20">
+              <p className="text-4xl lg:text-6xl 2xl:text-7xl font-extrabold text-center text-black dark:text-white">
+                Smart Study Space
               </p>
-              <span className="flex gap-1 py-1 px-3 text-lg lg:text-2xl text-gray-600 dark:text-gray-300">
+              <span className="text-center text-lg lg:text-2xl text-gray-600 dark:text-gray-300">
                 Welcome Back - Please login to your account to continue
               </span>
             </div>
           </div>
 
-          <div className="w-full lg:w-1/3 p-4 lg:p-1 flex flex-col justify-center items-center">
+          <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
             <form
               onSubmit={onSubmit}
-              className="w-full lg:w-[400px] flex flex-col gap-y-4 px-10 pt-14 pb-14"
+              className="w-full max-w-sm lg:w-[400px] flex flex-col gap-y-4 px-6 sm:px-10 pt-14 pb-14"
             >
-              <div className="">
+              <div className="text-center flex flex-col items-center justify-center">
                 <img
                   src={logo}
                   alt="BK TPHCM Logo"
-                  className="justify-self-center text-center items-center justify-center w-24 h-24 mb-4"
+                  className="w-24 h-24 mb-4 mx-auto"
                 />
-                <p className="font-bold text-2xl text-black dark:text-white text-center">Login</p>
+                <p className="font-bold text-2xl text-black dark:text-white">
+                  Login
+                </p>
               </div>
+
               <div className="flex flex-col gap-y-2">
                 <div className="w-full flex flex-col gap-0 ">
                   <label className="text-left block text-gray-700 dark:text-gray-300 text-sm font-bold mb-1">
@@ -99,8 +102,8 @@ export default function Login() {
                   </label>
                   <input
                     type="text"
-                    id='username'
-                    name='username'
+                    id="username"
+                    name="username"
                     value={username}
                     className="w-full bg-white rounded-lg px-3 py-2.5 2xl:py-3 border border-gray-300 placeholder-gray-400 text-gray-900 outline-none text-base focus:ring-2 ring-blue-300"
                     placeholder="Username"
@@ -114,8 +117,8 @@ export default function Login() {
                   </label>
                   <input
                     type="password"
-                    id='password'
-                    name='password'
+                    id="password"
+                    name="password"
                     value={password}
                     className="w-full bg-white rounded-lg px-3 py-2.5 2xl:py-3 border border-gray-300 placeholder-gray-400 text-gray-900 outline-none text-base focus:ring-2 ring-blue-300"
                     placeholder="Password"
